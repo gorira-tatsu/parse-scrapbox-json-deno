78470f0eaa084c05008fcfa03b85074adf83bd3a
gorira <tatsuhiko.shigoto@gmail.com>
2023-12-20 23:57:43 +0900

    とりあえずいい感じに書いた

--- main.ts
+++ main.ts
@@ -1,21 +1,46 @@
 import data from "./data/gorira.json" with { type: "json" };
-const corner_array:string[] = [""]
 
+const corner_array:string[] = []
 const page_data = data.pages
 const matched_data = page_data.map(
     (page) => page.lines.slice(1).join().match(/#(?![^\[]*])[^\s,]+/)?.[0]
 )
 
-const corner_case = matched_data.filter(
-    (matched) => matched?.search(/`/) === -1
-)
+const LinkedAllTagPageid = () => {
+    let AllLinked = new Map([
 
-const unique_data = Array.from(new Set(corner_case.filter(
-    (matched) => matched !== undefined
-)))
+    ]);
 
-const filterd_data = unique_data.filter(
-    (data) =>  data !== undefined && !corner_array.includes(data)
-)
+    page_data.map(
+        (page) => {
+            const CheckTheTag = page.lines.slice(1).join().match(/#(?![^\[]*])[^\s,]+/)?.[0]
+            if (CheckTheTag && !AllLinked.has(CheckTheTag)) {
+                const LinkedTagPageids: string[] = [String(page.id)]
+                AllLinked.set(CheckTheTag, LinkedTagPageids)
+            }
+            else if (CheckTheTag && AllLinked.has(CheckTheTag)) {
+                const LinkedTagPageids: string[] = Array.from(AllLinked.get(CheckTheTag) || [])
+                LinkedTagPageids.push(String(page.id));
+                AllLinked.set(CheckTheTag, LinkedTagPageids)
+            }
+        }
+    )
 
-console.log(filterd_data)
+    return AllLinked
+}
+
+
+console.log(LinkedAllTagPageid().get("#見た記事").length!)
+
+// const corner_case = matched_data.filter(
+//     (matched) => matched?.search(/`/) === -1
+// )
+//
+// const unique_data = corner_case.filter(
+//     (matched) => matched !== undefined
+// )
+//
+// const filterd_data = unique_data.filter(
+//     (data) =>  data !== undefined && !corner_array.includes(data)
+// )
+
