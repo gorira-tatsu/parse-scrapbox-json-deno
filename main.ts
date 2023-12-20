533b7cfc39fce508156b965a4c692c4c40ca8091
gorira <tatsuhiko.shigoto@gmail.com>
2023-12-20 23:59:56 +0900

    fixed

--- main.ts
+++ main.ts
@@ -1,10 +1,6 @@
 import data from "./data/gorira.json" with { type: "json" };
 
-const corner_array:string[] = []
 const page_data = data.pages
-const matched_data = page_data.map(
-    (page) => page.lines.slice(1).join().match(/#(?![^\[]*])[^\s,]+/)?.[0]
-)
 
 const LinkedAllTagPageid = () => {
     let AllLinked = new Map();
@@ -28,17 +24,4 @@ const LinkedAllTagPageid = () => {
 };
 
 
-console.log(LinkedAllTagPageid().get("#見た記事").length!)
-
-// const corner_case = matched_data.filter(
-//     (matched) => matched?.search(/`/) === -1
-// )
-//
-// const unique_data = corner_case.filter(
-//     (matched) => matched !== undefined
-// )
-//
-// const filterd_data = unique_data.filter(
-//     (data) =>  data !== undefined && !corner_array.includes(data)
-// )
-
+console.log(LinkedAllTagPageid())
